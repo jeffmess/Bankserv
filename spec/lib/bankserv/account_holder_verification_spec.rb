b3d7ecde4873c34c465522354096a3428f11d7e0
Douglas Anderson <i.am.douglas.anderson@gmail.com>
2012-01-04 16:35:35 +0200

    Split spec

--- spec/lib/bankserv/account_holder_verification_spec.rb
+++ spec/lib/bankserv/account_holder_verification_spec.rb
@@ -2,38 +2,41 @@ require 'spec_helper'
 
 describe Bankserv::AccountHolderVerification do
   
-  before(:each) do
+  context "requesting an account holder verification" do
     
-  end
+    before(:all) do
+      @bank_hash = {
+        account_number: "2938423984",
+        branch_code: "250255",
+        account_type: 'savings',
+        id_number: '0394543905',
+        initials: "P",
+        account_name: "Hendrik"
+      }
+    
+      @hash = {
+        type: 'ahv',
+        data: {user_ref: "34"}.merge(@bank_hash)
+      }
+    end
   
-  it "should be able to queue a request for an account holder verification" do
-    bank_hash = {
-      account_number: "2938423984",
-      branch_code: "250255",
-      account_type: 'savings',
-      id_number: '0394543905',
-      initials: "P",
-      account_name: "Hendrik"
-    }
+    it "should be able to queue a request for an account holder verification" do
+      Bankserv::AccountHolderVerification.request(@hash).should be_true
     
-    hash = {
-      type: 'ahv',
-      data: {user_ref: "34"}.merge(bank_hash)
-    }
-    
-    Bankserv::AccountHolderVerification.request(hash).should be_true
-    
-    request = Bankserv::Request.last
-    request.type.should == "ahv"
-    request.data.should == hash[:data]
-    
-    ahv = Bankserv::AccountHolderVerification.for_reference(hash[:data][:user_ref]).first
-    ahv.processed.should be_false
-    
-    bank_hash.each do |k,v|
-      ahv.bank_account.send(k).should == v
+      request = Bankserv::Request.last
+      request.type.should == "ahv"
+      request.data.should == @hash[:data]
     end
     
+    it "should create an account holder verification record, with associated bank account" do
+      Bankserv::AccountHolderVerification.request(@hash).should be_true
+      
+      ahv = Bankserv::AccountHolderVerification.for_reference(@hash[:data][:user_ref]).first
+      ahv.processed.should be_false
+    
+      @bank_hash.each{|k,v| ahv.bank_account.send(k).should == v}
+    end
+  
   end
       
 end
