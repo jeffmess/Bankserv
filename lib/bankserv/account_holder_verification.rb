3566316807bfb33daa88d34685437ee16118fa53
Douglas Anderson <i.am.douglas.anderson@gmail.com>
2012-01-05 13:46:52 +0200

    Attempt to use record templates provided by Absa::H2h gem

--- lib/bankserv/account_holder_verification.rb
+++ lib/bankserv/account_holder_verification.rb
@@ -41,34 +41,23 @@ module Bankserv
     end
     
     def to_hash
-      account_detail = {
-        rec_id: 31,
+      account_detail = if external?
+        Absa::H2h::Transmission::AccountHolderVerification.record_type('external_account_detail').template_options
+      else
+        Absa::H2h::Transmission::AccountHolderVerification.record_type('internal_account_detail').template_options
+      end
+        
+      account_detail.merge!(
         rec_status: "T",
         seq_no: 1,
         acc_no: bank_account.account_number,
         idno: bank_account.id_number,
         initials: bank_account.initials,
         surname: bank_account.account_name,
-        return_code_1: 0,
-        return_code_2: 0,
-        return_code_3: 0,
-        return_code_4: 0,
         user_ref: user_ref
-      }
+      )
       
-      if external?
-        account_detail.merge({
-          branch_code: bank_account.branch_code,
-          originating_bank: 60,
-          ld_code: "LD00000",
-          return_code_5: 0,
-          return_code_6: 0,
-          return_code_7: 0,
-          return_code_8: 0,
-          return_code_9: 0,
-          return_code_10: 0
-        })
-      end
+      account_detail.merge!(branch_code: bank_account.branch_code) if external?
     end
   end
   
